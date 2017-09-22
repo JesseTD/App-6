c04d6cf0119b138ffc65636a0588e16ab0a6e123
Jesse Dubbs <jessetd@live.com>
2017-09-22 12:22:24 -0400

    config replacement fix

--- server/config/app.js
+++ server/config/app.js
@@ -1,11 +1,11 @@
 require('dotenv').load();
-var config = require('./config'), 
+var /*config = require('./config'), */
     mongoose = require('mongoose'),   
     express = require('./express');
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
+  app.listen(process.env.port, function() {
     console.log('App listening on port', process.env.PORT);
   });
 };
